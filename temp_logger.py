e91d2f686d025dc82d95f1d420c483f65039a402
Wesley Blake <wesley@nikordi.com>
2017-03-01 14:14:17 +0200

    influxdb config

--- temp_logger.py
+++ temp_logger.py
@@ -1,11 +1,20 @@
 import os
 import time
-from os import path
+from influxdb import InfluxDBClient
+
+influx_host = os.getenv("INFLUX_HOST")
+port = 8086
+dbname = os.getenv("INFLUXDB_NAME")
+user = os.getenv("INFLUXDB_USER")
+password = os.getenv("INFLUXDB_PASS")
+temp_sensor = os.environ.get("SENSOR")
+
+client = InfluxDBClient(influx_host, port, user, password, dbname)
+client.create_database(dbname)
 
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
-temp_sensor = os.environ.get("SENSOR")
 
 def temp_raw():
     f = open(temp_sensor, 'r')
@@ -24,7 +33,29 @@ def read_temp():
         temp_c = float(temp_string) / 1000.0
         return temp_c
 
-while True:
-        print(read_temp())
-        time.sleep(1)
 
+def get_data_points():
+    temperature = read_temp()
+
+    iso = time.ctime()
+    json_body = [
+            {
+                "measurement": "vessel_celcius",
+                "tags": {"host": host},
+                "time": iso,
+                "fields": {
+                    "value": temperature,
+                    "val": float(temperature)
+                    }
+                }
+
+            ]
+
+    return json_body
+
+
+while True:
+    json_body = get_data_points()
+    client.write_points(json_body)
+    print (".")
+    time.sleep(sleep_time)
